7f78f675e2fde0de8cef180229a2f9b399dae8e9
ibrahim3ziz <Ibrahim.aziz1198@gmail.com>
2025-06-27 05:40:31 +0300

    Initial Commit

---
+++ Pleny/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pleny
+//
+//  Created by Ibrahim Abdul Aziz on 27/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pleny/PlenyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlenyApp.swift
+//  Pleny
+//
+//  Created by Ibrahim Abdul Aziz on 27/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlenyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
