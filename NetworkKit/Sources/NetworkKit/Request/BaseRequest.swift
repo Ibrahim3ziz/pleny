83ba17dbd6624d48ab9ca54b8bdb194684d4b283
ibrahim3ziz <Ibrahim.aziz1198@gmail.com>
2025-06-27 18:40:58 +0300

    add Base request
    create the url

---
+++ NetworkKit/Sources/NetworkKit/Request/BaseRequest.swift
@@ -0,0 +1,85 @@
+//
+//  BaseRequest.swift
+//  NetworkKit
+//
+//  Created by Ibrahim Abdul Aziz on 27/06/2025.
+//
+
+import Foundation
+
+public protocol BaseRequest {
+    var baseURL: String { get }
+    var scheme: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String: String]? { get }
+    var parameter: [String: String]? { get }
+    var body: [String: String]? { get }
+}
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
+
+extension BaseRequest {
+    
+    var scheme: String {
+        return "https"
+    }
+    
+    var baseURL: String {
+        return ""
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var headers: [String: String]? {
+        return [:]
+    }
+    
+    var parameter: [String: String]? {
+        return [:]
+    }
+    
+    var body: [String: String]? {
+        return [:]
+    }
+}
+
+
+extension BaseRequest {
+    public func asURLRequest() -> URLRequest {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = baseURL
+        components.path = "/" + path
+        
+        if let parameter = parameter, !parameter.isEmpty {
+            components.queryItems = parameter.map { URLQueryItem(name: $0.key, value: $0.value) }
+        }
+        
+        guard let url = components.url else {
+            preconditionFailure("❌ Invalid URL: \(components)")
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        
+        if let headers = headers {
+            for (key, value) in headers {
+                request.addValue(value, forHTTPHeaderField: key)
+            }
+        }
+        
+        if let body = body {
+            request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
+        }
+        return request
+    }
+}
