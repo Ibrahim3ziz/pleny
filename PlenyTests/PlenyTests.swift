210de3f87f4692469414fc09a5b3c494cb933ed7
ibrahim3ziz <Ibrahim.aziz1198@gmail.com>
2025-06-30 18:45:17 +0300

    add unit tests for get post

--- PlenyTests/PlenyTests.swift
+++ PlenyTests/PlenyTests.swift
@@ -6,31 +6,112 @@
 //
 
 import XCTest
+import Combine
+import NetworkKit
 @testable import Pleny
 
 final class PlenyTests: XCTestCase {
-
+    
+    private var viewModel: HomeViewModel!
+    private var mockUseCase: MockHomeUseCase!
+    private var cancellables: Set<AnyCancellable>!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        mockUseCase = MockHomeUseCase()
+        viewModel = HomeViewModel(useCase: mockUseCase)
+        cancellables = []
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mockUseCase = nil
+        viewModel = nil
+        cancellables = nil
+        try super.tearDownWithError()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    // MARK: - Test Success
+    
+    func test_getPosts_success_shouldUpdatePosts() {
+        // Given
+        let expectedPosts = [
+            Post(
+                id: 1,
+                title: "Test",
+                body: "Hello",
+                tags: ["news"],
+                reactions: Reactions(likes: 10, dislikes: 1),
+                views: 100,
+                userID: 1),
+            Post(
+                id: 2,
+                title: "Another",
+                body: "World",
+                tags: ["tech"],
+                reactions: Reactions(likes: 5, dislikes: 0),
+                views: 50,
+                userID: 2
+            )
+        ]
+        
+        let response = PostsEntity(
+            posts: expectedPosts,
+            total: 2,
+            skip: 0,
+            limit: 10
+        )
+        
+        mockUseCase.fetchPostsResult = Just(response)
+            .setFailureType(to: NetworkError.self)
+            .eraseToAnyPublisher()
+        
+        let expectation = XCTestExpectation(description: "Posts updated")
+        
+        // When
+        viewModel.$posts
+            .dropFirst()
+            .sink { posts in
+                XCTAssertEqual(posts.count, 2)
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        viewModel.getPosts()
+        
+        // Then
+        wait(for: [expectation], timeout: 1.0)
     }
-
+    
+    // MARK: - Performance (Optional)
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        mockUseCase.fetchPostsResult = Just(PostsEntity(posts: [], total: 0, skip: 0, limit: 10))
+            .setFailureType(to: NetworkError.self)
+            .eraseToAnyPublisher()
+        
+        measure {
+            let expectation = XCTestExpectation(description: "Wait for posts")
+            
+            viewModel.$posts
+                .dropFirst()
+                .sink { _ in
+                    expectation.fulfill()
+                }
+                .store(in: &cancellables)
+            
+            viewModel.getPosts()
+            wait(for: [expectation], timeout: 1.0)
         }
     }
-
+    
+    func testExample() {
+        // Given
+        let firstNumber = 10
+        let secondNumber = 5
+        
+        // When
+        let result = firstNumber + secondNumber
+        
+        // Then
+        XCTAssertEqual(result, 15)
+    }
 }
