ef36d5fb60b912dd56167c01169c8a166b3c7585
ibrahim3ziz <Ibrahim.aziz1198@gmail.com>
2025-06-30 03:08:58 +0300

    update naming

--- NetworkKit/Sources/NetworkKit/NetworkManager/NetworkSessionInterface.swift
+++ NetworkKit/Sources/NetworkKit/NetworkManager/NetworkSessionInterface.swift
@@ -9,7 +9,7 @@ import Foundation
 import Combine
 
 /// Abstract interface for a network session, allowing for mocking/testing and dependency injection.
-public protocol NetworkSessionInterface {
+public protocol NetworkServiceInterface {
     /// Executes a network request and returns a publisher with a decoded result or a `NetworkError`.
     func execute<T: Codable>(_ request: BaseRequest, model: T.Type) -> AnyPublisher<T, NetworkError>
 }
